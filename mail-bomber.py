0113a6413db73e6eb7600caadb292d17172d7465
ZiksthemW <47642797+ZiksthemW@users.noreply.github.com>
2021-04-21 10:24:58 +0300

    Translated to EN

--- mail-bomber.py
+++ mail-bomber.py
@@ -5,23 +5,23 @@ mail_gonderildi = 0
 
 while not servis_girildi:
     mail_servisi = int(input(f"""{cizgi}
-    Mevcut servisler: (1) GMail / (2) Yahoo / (3) Outlook / (4) Yandex Mail
-        Kullanmak istediğiniz servisi <> """))
+    Services: (1) GMail / (2) Yahoo / (3) Outlook / (4) Yandex Mail
+        Choose the service that you want to use <> """))
 
     if mail_servisi <= 0 or mail_servisi >= 5:
-        print(f"{cizgi}\nHatalı giriş. Lütfen sadece (1) ila (4) arası giriş yapınız.")
+        print(f"{cizgi}\nIncorrect entry. Please choose between (1) and (4).")
     else:
         servis_girildi = True
 
-mail_gonderilecek = int(input(f"{cizgi}\nKaç kere spam atmak istiyorsunuz? <> "))
-mail_kadi = str(input(f"{cizgi}\nMail Adresiniz <> "))
-mail_pass = str(input(f"{cizgi}\nMail Parolanız <> "))
-mail_alici = str(input(f"{cizgi}\nMail Alıcısı <> "))
-mail_baslik = str(input(f"{cizgi}\nMailiniz için konu başlığı giriniz <> "))
-mail_icerik = str(input(f"{cizgi}\nSADECE DOSYA ADI GİRİLMELİ.\nMail içeriğinin bulunduğu .txt dosyasını belirtiniz <> "))
+mail_gonderilecek = int(input(f"{cizgi}\nHow much times do you want to spam? <> "))
+mail_kadi = str(input(f"{cizgi}\nYour Mail Address <> "))
+mail_pass = str(input(f"{cizgi}\nYour Mail Password <> "))
+mail_alici = str(input(f"{cizgi}\nMail Receiver <> "))
+mail_baslik = str(input(f"{cizgi}\nMail Subject <> "))
+mail_icerik = str(input(f"{cizgi}\nWITHOUT FILE EXTENSION.\nInput the .txt file that has the mail content <> "))
 print(cizgi)
 
-if not mail_gonderilecek or mail_kadi or mail_pass or mail_alici or mail_baslik or mail_icerik == None: # Burası bayağı uzun oldu, yakında daha kısası ile çözerim.
+if not mail_gonderilecek or mail_kadi or mail_pass or mail_alici or mail_baslik or mail_icerik == None: # Will change there soon.
     with open(mail_icerik + ".txt", "r", encoding="utf-8") as mail_icerik:
         mail_icerik_veri = mail_icerik.read()
         mail_icerik_veri = f"""SUBJECT: {mail_baslik}\n{mail_icerik_veri}"""
@@ -40,10 +40,10 @@ if not mail_gonderilecek or mail_kadi or mail_pass or mail_alici or mail_baslik 
         mail.starttls()
         mail.login(mail_kadi, mail_pass)
         mail.sendmail(mail_baslik, mail_alici, mail_icerik_veri)
-        print(f"Başarıyla yollandı. Kalan gönderim: {mail_gonderilecek - mail_gonderildi}")
+        print(f"Successfully sent the mail. {mail_gonderilecek - mail_gonderildi} Mails left to send")
         mail_gonderildi += 1
 
-    print(f"{cizgi}\nMailler başarı ile {mail_gonderildi} kez gönderildi.")
+    print(f"{cizgi}\nSuccessfully sent {mail_gonderildi} mails.")
 
 else:
-    print(f"{cizgi}\nEksik bilgi girildi.")
+    print(f"{cizgi}\nMissing information provided.")
